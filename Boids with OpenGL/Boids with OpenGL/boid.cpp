b3a535bfc1edc802ad6d321b864fe3cacb308dd6
Julia Green <jcgreen2@ualr.edu>
2021-03-30 15:20:02 -0500

    Began working on storing boid verticies

--- Boids with OpenGL/Boids with OpenGL/boid.cpp
+++ Boids with OpenGL/Boids with OpenGL/boid.cpp
@@ -11,18 +11,25 @@ Boid::Boid() {
 
 	//Assigns random position for first vertex
 	std::uniform_int_distribution<float> randomPosition(0, screenWidth - 25);
-	vertPositions.at(0) = glVertex2f(randomPosition(randEngine), randomPosition(randEngine));
+	float x = randomPosition(randEngine);
+	float y = randomPosition(randEngine);
+	vertPositions.at(0) = glVertex2f(x, y);
 
 	//Calculates positon for other two verticies
-	if (velocity.at(0) >= 0 && velocity.at(1) >= 0) {
-		vertPositions.at(0) = glVertex2f(randomPosition(randEngine), randomPosition(randEngine));
-	
-	} else if(velocity.at(0) >= 0 && velocity.at(1) < 0 || velocity.at(0) > 0 && velocity.at(1) <= 0) {
-		
-	} else if (velocity.at(0) <= 0 && velocity.at(1) > 0 || velocity.at(0) < 0 && velocity.at(1) >= 0) {
-		
-	} else {
-			
+	// x = (y + 4) / 2, y = 2x - 4
+	// x = (y - 8) / -2, y = -2x + 8
+	if (velocity.at(0) >= 0 && velocity.at(1) >= 0) { // Both x and y velocities are positive or both 0
+		vertPositions.at(1) = glVertex2f((y + 4) / 2, (2 * x) - 4);
+		vertPositions.at(2) = glVertex2f((y - 8) / -2, (-2 * x) + 8);
+	} else if(velocity.at(0) >= 0 && velocity.at(1) < 0 || velocity.at(0) > 0 && velocity.at(1) <= 0) { // X velocity is positive/0 and y velocity is negative or X velocity is positive and y velocity is negative/0
+		vertPositions.at(1) = glVertex2f(x - 10.0, y - 5);
+		vertPositions.at(2) = glVertex2f(x - 10.0, y + 5);
+	} else if (velocity.at(0) <= 0 && velocity.at(1) > 0 || velocity.at(0) < 0 && velocity.at(1) >= 0) { // X velocity is negative/0 and y velocity is positive or X velocity is negative and y velocity is positive/0
+		vertPositions.at(1) = glVertex2f(x - 10.0, y - 5);
+		vertPositions.at(2) = glVertex2f(x - 10.0, y + 5);
+	} else { // Both x and y velocities are negative
+		vertPositions.at(1) = glVertex2f(x - 10.0, y - 5);
+		vertPositions.at(2) = glVertex2f(x - 10.0, y + 5);
 	}
 
 }
